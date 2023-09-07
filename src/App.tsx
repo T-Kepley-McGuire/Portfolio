c4f8fe4480a57eafdaeb5a13a2b5865274c89dbe
T-Kepley-McGuire <kepley.mcguire.mail@gmail.com>
2023-09-06 18:56:10 -0700

    initial commit. Main page rendering fine, very little content, router mid-setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="bg"></div>
+      <div className="app">
+        <header>
+          <Link to="/about">About</Link>
+          <Link to="/projects">Projects</Link>
+          <Link to="/blog">Blog</Link>
+          <Link to="/contact">Contact</Link>
+        </header>
+        <main>
+          <div className="title">
+            <div>
+              <h1>T. Kepley McGuire</h1>
+              <p>I'm a web developer and rendering enthusiast</p>
+              <p>Check out my work here</p>
+            </div>
+          </div>
+          <div className="content">
+            <h3>Who I am</h3>
+            <p>
+              When I stumbled into coding, I knew I had found a passion. As a
+              former math enthusiast, coding was an extension of math that I
+              could realize onto a computer screen. Although my introduction to
+              coding was through a class at Eastern Washington University, I
+              quickly converted it into my new favorite hobby. It wasn't long
+              before I was investigating algorithms for calculating the prime
+              numbers and the fibbinocci sequence.
+            </p>
+            <p>
+              Eventually, I found the game engine Unity. My love for games
+              combined with my love of coding and I started making games. My
+              naivety helped me to persevere, not realizing how large of task I
+              took on. I prototyped and scrapped tens of projects. During this
+              time, I began experimenting with neural networks as well. I pushed
+              my abilities as far as they would go and farther.
+            </p>
+            <p>
+              Eventually, I felt the need to expand my hobby to a professional
+              enviroment. My time teaching myself to code would be indespensible
+              for picking up the new domain of Web Development. I enrolled in an
+              online coding academy: Thinkful. Through my time at Thinkful, I
+              picked up all the professional skills one would need to succeed in
+              the job place. Full stack development through React.js and
+              Express.js prepared me for the professional standard of web app
+              engineering
+            </p>
+          </div>
+        </main>
+        <footer>
+          <div className="filler"></div>
+        </footer>
+      </div>
     </div>
   );
 }
